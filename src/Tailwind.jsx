a5760f6550e60bd8a47792c7690b6b8d5f2c8345
Hari-repo <hariharanuk810@gmail.com>
2025-10-23 19:41:16 +0530

    Inital commit in reactjs with tailwind css added

---
+++ src/Tailwind.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tailwind = () => {
+    return(
+        <>
+            <h1 className="text-3xl">Hello Tailwind</h1>
+            <h2>Hello Tailwind</h2>
+            <h3>Hello Tailwind</h3>
+            <h4>Hello Tailwind</h4>
+            <h5>Hello Tailwind</h5>
+            <h6>Hello Tailwind</h6>
+        </>
+    )
+}
+
+export default Tailwind;
