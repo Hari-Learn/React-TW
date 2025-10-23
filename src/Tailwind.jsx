17dc96a10151e3b248d076828eeb2f28e5c74fb6
Hari-repo <hariharanuk810@gmail.com>
2025-10-23 19:45:22 +0530

    Removed boilerplate codes

--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Tailwind = () => {
     return(
         <>
-            <h1 className="text-3xl">Hello Tailwind</h1>
-            <h2>Hello Tailwind</h2>
-            <h3>Hello Tailwind</h3>
-            <h4>Hello Tailwind</h4>
-            <h5>Hello Tailwind</h5>
-            <h6>Hello Tailwind</h6>
+            <h1 className="text-2xl">Hello Tailwind</h1>
+            <h2 className="text-3xl">Hello Tailwind</h2>
+            <h3 className="text-4xl">Hello Tailwind</h3>
+            <h4 className="text-5xl">Hello Tailwind</h4>
+            <h5 className="text-6xl">Hello Tailwind</h5>
+            <h6 className="text-7xl">Hello Tailwind</h6>
         </>
     )
 }
