295ffb2a6291b34f7ff2f27a9ed6b51c9d2955da
Hari-repo <hariharanuk810@gmail.com>
2025-10-23 20:29:51 +0530

    condition render added

--- src/comp/Dashboard.jsx
+++ src/comp/Dashboard.jsx
@@ -2,8 +2,17 @@
 const Dashboard = () => {
     const isLoggedIn = true;
     return(
-        <div>
-            {isLoggedIn ? "true" : "false"}
+        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            {isLoggedIn ? 
+            <h4 className="text-5xl">True</h4> : 
+            <h4 className="text-5xl">False</h4>}
+
+            {isLoggedIn && (
+                <>
+                <h2>Please Log In</h2>
+                </>
+                )
+            }
         </div>
     )
 }
