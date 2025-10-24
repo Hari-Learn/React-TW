cd47371cff2df1ba6ab0c2cc032762f49eca86ec
Hari-repo <hariharanuk810@gmail.com>
2025-10-24 20:37:42 +0530

    tailwind card is added

--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -10,6 +10,12 @@ const LogIn = () =>{
          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
             Log In - {count}
         </button>
+
+        
+
+
+
+
         </>
     )
 }
