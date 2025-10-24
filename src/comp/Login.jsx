1f1fd8944302698760b947364bd0a0eab07a413d
Hari-repo <hariharanuk810@gmail.com>
2025-10-24 20:30:54 +0530

    Inital useState defined with btn

--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 const LogIn = () =>{
+    const [count,setCount] = useState(0);
     return (
         <>
         <div>Gotcha  Login Page</div>
+        <button
+         onClick={()=>setCount(count+1)}
+         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Log In - {count}
+        </button>
         </>
     )
 }
