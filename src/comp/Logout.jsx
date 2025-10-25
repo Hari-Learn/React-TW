d6b975b8e837d163e3bffbea2bed1dca2c32cfbe
Hari-repo <hariharanuk810@gmail.com>
2025-10-25 22:27:45 +0530

    logout button added in login comp

---
+++ src/comp/Logout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logout = () => {
+    return(
+         <button                
+                className="px-4 py-2 bg-red-300 text-white rounded hover:bg-red-400">
+                Logout
+            </button>
+    )
+}
+
+export default Logout
