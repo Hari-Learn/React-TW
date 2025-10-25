163080a7489a8a7dc6e9bff753e6875c1d977c0c
Hari-repo <hariharanuk810@gmail.com>
2025-10-25 22:30:26 +0530

    Login forms basic added

--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -5,6 +5,9 @@ const LogIn = () => {
     return (
         <>
             <div>Gotcha  Login Page</div>
+            <input type="text" ></input>
+             <input type="password" ></input>
+             <br/>
             <button
                 onClick={() => setCount(count + 1)}
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
