704a0e95d7d0da911234d7cb8fe4fb26e54882c2
Hari-repo <hariharanuk810@gmail.com>
2025-10-26 19:27:49 +0530

    Navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Dashboard from './comp/Dashboard.jsx'
+import NavBar from './comp/NavBar.jsx'
 import Tailwind from './Tailwind.jsx'
 function App() {
  
 
   return (
     <>
+    <NavBar></NavBar>
     <Tailwind></Tailwind>
      
       <Dashboard></Dashboard>
