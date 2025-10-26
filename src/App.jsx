c37af922d5001aa1068937e48c98ba490b9d3592
Hari-repo <hariharanuk810@gmail.com>
2025-10-26 19:39:22 +0530

    Tab component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 import Dashboard from './comp/Dashboard.jsx'
 import NavBar from './comp/NavBar.jsx'
+import Tab from './comp/Tab.jsx'
 import Tailwind from './Tailwind.jsx'
 function App() {
- 
+
 
   return (
     <>
-    <NavBar></NavBar>
-    <Tailwind></Tailwind>
-     
+      <NavBar></NavBar>
+      <Tailwind></Tailwind>
+
       <Dashboard></Dashboard>
-      
+      <center>
+      <Tab></Tab>
+      </center>
+
     </>
   )
 }
