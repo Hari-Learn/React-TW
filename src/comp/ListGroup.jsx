62e54c7e2f7edb90f7b57edba5e3b590b4a3cc97
Hari-repo <hariharanuk810@gmail.com>
2025-10-27 20:49:37 +0530

    Added delete button with list of arrays with useState

--- src/comp/ListGroup.jsx
+++ src/comp/ListGroup.jsx
@@ -2,13 +2,32 @@ import React, { useState } from "react";
 
 const ListGroup = ()=>{
     const [tasks, setTasks] = useState(['Learn','Build','Test'])
+
+    const removeTask = (index) =>{
+        setTasks(tasks.filter((_,i)=> i!= index));        
+    }
     return(
-        <div className="flex justify-center">
-            <h2>Tasks</h2>
-            {tasks.map(item => (
-                <h3>{"   -  "+item+ " "}</h3>
-            ))}
-        </div>
+        <>
+       
+
+        <div className="flex flex-col items-center"> <h2>Tasks</h2> 
+        
+
+
+
+<ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg divide-y divide-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:divide-gray-600 dark:text-white">
+  {tasks.map((item, index) => (
+    <li key={index}
+      className="flex items-center justify-between w-full px-4 py-2" >
+      <span>{item}</span>
+      <button
+        className="bg-red-500 hover:bg-red-600 text-white text-xs px-2 py-1 rounded"
+        onClick={() => removeTask(index)}> Delete   </button>
+    </li>
+  ))}
+</ul></div>
+</>
+
     )
 }
 
