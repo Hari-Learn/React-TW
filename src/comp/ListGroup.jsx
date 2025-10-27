b3d7fe9bb3936a2cd5311e3d5afa97e192b885f4
Hari-repo <hariharanuk810@gmail.com>
2025-10-27 20:17:48 +0530

    useState used in listgroup

--- src/comp/ListGroup.jsx
+++ src/comp/ListGroup.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListGroup = ()=>{
+    const [tasks, setTasks] = useState(['Learn','Build','Test'])
     return(
         <div className="flex justify-center">
             <h2>Tasks</h2>
+            {tasks.map(item => (
+                <h3>{"   -  "+item+ " "}</h3>
+            ))}
         </div>
     )
 }
